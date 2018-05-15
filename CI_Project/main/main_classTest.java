85565a06d02ab3789f8fcca8eb51ed52eb5a04cf
Andriy Tsap <ats@softserveinc.com>
2018-05-15 10:46:35 +0300

    test 1

---
+++ CI_Project/main/main_classTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class main_classTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
