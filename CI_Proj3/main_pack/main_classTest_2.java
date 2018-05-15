e665d01c6041605bcdf8e4bee032727c62df3efb
Andriy Tsap <ats@softserveinc.com>
2018-05-15 13:15:25 +0300

    sdfgh

--- CI_Proj3/main_pack/main_classTest_2.java
+++ CI_Proj3/main_pack/main_classTest_2.java
@@ -1,14 +1,14 @@
-//package main_pack;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Test;
-//
-//public class main_classTest_2 {
-//
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
-//
-//}
+package main_pack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class main_classTest_2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
