a49ded597d1c76a00bcee5117bc261da7b66fd62
Andriy Tsap <ats@softserveinc.com>
2018-05-15 10:50:54 +0300

    dfgh

---
+++ CI_Project/main/main_classTest_2.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class main_classTest_2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
